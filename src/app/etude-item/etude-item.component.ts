c0bc5fb121d4ba8f4e276baf2f3dbc5e988f516b
Walid <cartman010@live.fr>
2021-08-03 09:28:10 +0100

    03/08/2021

--- src/app/etude-item/etude-item.component.ts
+++ src/app/etude-item/etude-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-etude-item',
@@ -12,15 +12,31 @@ export class EtudeItemComponent implements OnInit {
   title 
   @Input()
   display
+  @Input()
+  validDates
 
   niveaux = ['Secondaire/Bac', 'Licence (B+3)', 'Master (B+5)', 'Doctorat (B+8/11)']
+  years = []
+
   niveauControl: FormControl
+  anneeDebutControl: FormControl
+  anneeFinControl: FormControl
+
+  childrenEqual: ValidatorFn = (formGroup: FormGroup) => {
+    const [anneeDebutControl, ...anneeFinControl] = Object.keys(formGroup.controls || {});
+    const isValid = anneeFinControl.every(controlName => formGroup.get(controlName).value === formGroup.get(anneeDebutControl).value);
+    return isValid ? null : { childrenNotEqual: true };
+  }
 
   constructor() { }
 
   ngOnInit(): void {
     this.niveauControl = new FormControl(this.title);
+    this.anneeDebutControl = new FormControl(new Date().getFullYear().toString(), this.childrenEqual);
+    this.anneeFinControl = new FormControl(new Date().getFullYear().toString());
     this.display = true
+    this.validDates = true
+    this.setYears()
   }
 
   onClickRemove() {
@@ -29,6 +45,16 @@ export class EtudeItemComponent implements OnInit {
 
   setTitle() {
     this.title = this.niveauControl.value
+    console.log("this.anneeDebutControl.value >= this.anneeFinControl.value" +
+      this.anneeDebutControl.value + " ****** " + this.anneeFinControl.value)
+    if (Number.parseInt(this.anneeDebutControl.value) >= Number.parseInt(this.anneeFinControl.value))
+      this.validDates = false
+    else
+      this.validDates = true
   }
 
+  setYears() {
+    for (var i = 1980; i <= new Date().getFullYear(); i++)
+      this.years.push(i)
+  }
 }
